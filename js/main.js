5507ebd8e5908e75916d024c0eaab0a0e6e723bd
Fatimasah <fatimasahmoud@hotmail.com>
2020-09-03 23:57:09 +0300

    some modification on navbar and slider & fonts in portfolio section & create testimonials and spaces section

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$("document").ready(function($) {
+    $(function() {
+
+        $(".flipster").flipster({
+            style: 'carousel',
+            enableNav: false,
+        });
+
+    });
+    // navbar change background
+    // var nav = $('#navbar');
+    // $(window).scroll(function() {
+
+    //     if ($(this).scrollTop() > 0) {
+    //         nav.addClass("sticky");
+    //     } else {
+    //         nav.removeClass("sticky");
+    //     }
+    // });
+})
